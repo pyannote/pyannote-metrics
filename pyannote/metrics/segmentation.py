39f01afec1eca9c51127ef3a0892f5949e345d85
Herve Bredin <bredin@limsi.fr>
2014-05-19 12:01:04 +0200

    feat(segmentation): merge PK and WindowDiff
    @guinaudeau can you please check that the changes I made are correct?

--- pyannote/metrics/segmentation.py
+++ pyannote/metrics/segmentation.py
@@ -25,6 +25,7 @@
 
 from __future__ import unicode_literals
 
+import numpy as np
 from base import BaseMetric
 from pyannote.core import Annotation
 
@@ -32,6 +33,11 @@ PURITY_NAME = 'segmentation purity'
 COVERAGE_NAME = 'segmentation coverage'
 PTY_CVG_TOTAL = 'total duration'
 PTY_CVG_INTER = 'intersection duration'
+
+PK_NAME = 'segmentation pk'
+WINDOWDIFF_NAME = 'segmentation windowdiff'
+PK_AGREEMENT = 'number of agreements'
+PK_COMPARISON = 'number of comparisons'
 
 
 class SegmentationCoverage(BaseMetric):
@@ -143,6 +149,192 @@ class SegmentationPurity(SegmentationCoverage):
         return super(SegmentationPurity, self)._get_details(
             hypothesis, reference, **kwargs
         )
+
+
+class SegmentationPK(BaseMetric):
+    """Segmentation PK
+
+    Parameters
+    ----------
+    step : float, optional
+        Step in seconds. Defaults to 1.
+
+    Example
+    -------
+
+    >>> from pyannote.core import Timeline, Segment
+    >>> from pyannote.metrics.segmentation import SegmentationPK
+    >>> pk = SegmentationPK()
+
+    >>> reference = Timeline()
+    >>> reference.add(Segment(0, 1))
+    >>> reference.add(Segment(1, 2))
+    >>> reference.add(Segment(2, 4))
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 4))
+    >>> pk(reference, hypothesis)
+    1.0
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 3))
+    >>> hypothesis.add(Segment(3, 4))
+    >>> pk(reference, hypothesis)
+    0.75
+    """
+
+    def __init__(self, step=1):
+
+        super(SegmentationPK, self).__init__()
+        self.step = step
+
+    @classmethod
+    def metric_name(cls):
+        return PK_NAME
+
+    @classmethod
+    def metric_components(cls):
+        return [PK_AGREEMENT, PK_COMPARISON]
+
+    def _get_details(self, reference, hypothesis, **kwargs):
+
+        if isinstance(reference, Annotation):
+            reference = reference.get_timeline()
+
+        if isinstance(hypothesis, Annotation):
+            hypothesis = hypothesis.get_timeline()
+
+        detail = self._init_details()
+
+        # (half-)average duration of hypothesis segments
+        K = 0.5 * (hypothesis.duration() / len(hypothesis))
+
+        comparisons = 0.
+        agreements = 0.
+
+        start, end = hypothesis.extent()
+        for t in np.arange(start, end - K, self.step):
+
+            # true if t and t+K fall in the same segment
+            # false if t and t+K fall into 2 different segments
+            r = reference.overlapping(t) == reference.overlapping(t + K)
+            h = hypothesis.overlapping(t) == hypothesis.overlapping(t + K)
+
+            # increment if reference and hypothesis agree
+            agreements += (r == h)
+
+            comparisons += 1
+
+        detail[PK_AGREEMENT] += agreements
+        detail[PK_COMPARISON] += comparisons
+
+        return detail
+
+    def _get_rate(self, detail):
+
+        return 1. * detail[PK_AGREEMENT] / detail[PK_COMPARISON]
+
+    def _pretty(self, detail):
+        string = ""
+        string += "  - number of comparisons: %d\n" % (detail[PK_COMPARISON])
+        string += "  - number of agreements: %d\n" % (detail[PK_AGREEMENT])
+        string += "  - %s: %.2f %%\n" % (self.name, 100 * detail[self.name])
+        return string
+
+
+class SegmentationWindowDiff(BaseMetric):
+    """Segmentation WindowDiff
+
+    Parameters
+    ----------
+    step : float, optional
+        Step in seconds. Defaults to 1.
+
+    Example
+    -------
+
+    >>> from pyannote.core import Timeline, Segment
+    >>> from pyannote.metrics.segmentation import SegmentationWindowDiff
+    >>> wd = SegmentationWindowDiff()
+
+    >>> reference = Timeline()
+    >>> reference.add(Segment(0, 1))
+    >>> reference.add(Segment(1, 2))
+    >>> reference.add(Segment(2, 4))
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 4))
+    >>> wd(reference, hypothesis)
+    1.0
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 3))
+    >>> hypothesis.add(Segment(3, 4))
+    >>> wd(reference, hypothesis)
+    0.75
+    """
+
+    def __init__(self, step=1):
+        super(SegmentationWindowDiff, self).__init__()
+        self.step = step
+
+    @classmethod
+    def metric_name(cls):
+        return WINDOWDIFF_NAME
+
+    @classmethod
+    def metric_components(cls):
+        return [PK_AGREEMENT, PK_COMPARISON]
+
+    def _get_details(self, reference, hypothesis, **kwargs):
+
+        if isinstance(reference, Annotation):
+            reference = reference.get_timeline()
+
+        if isinstance(hypothesis, Annotation):
+            hypothesis = hypothesis.get_timeline()
+
+        detail = self._init_details()
+
+        # (half-)average duration of hypothesis segments
+        K = .5 * hypothesis.duration() / len(hypothesis)
+
+        comparisons = 0.
+        agreements = 0.
+
+        start, end = hypothesis.extent()
+        for t in np.arange(start, end - K, self.step):
+
+            # number of boundaries between t and t+K in reference
+            i = reference.index(reference.overlapping(t + K)[0])
+            j = reference.index(reference.overlapping(t)[0])
+            r = i - j
+
+            # number of boundaries between t and t+K in hypothesis
+            i = hypothesis.index(hypothesis.overlapping(t + K)[0])
+            j = hypothesis.index(hypothesis.overlapping(t)[0])
+            h = i - j
+
+            # increment if reference and hypothesis agree
+            agreements += (r == h)
+
+            comparisons += 1
+
+        detail[PK_AGREEMENT] += agreements
+        detail[PK_COMPARISON] += comparisons
+
+        return detail
+
+    def _get_rate(self, detail):
+
+        return 1. * detail[PK_AGREEMENT] / detail[PK_COMPARISON]
+
+    def _pretty(self, detail):
+        string = ""
+        string += "  - number of comparisons: %d\n" % (detail[PK_COMPARISON])
+        string += "  - number of agreements: %d\n" % (detail[PK_AGREEMENT])
+        string += "  - %s: %.2f %%\n" % (self.name, 100 * detail[self.name])
+        return string
 
 
 if __name__ == "__main__":
