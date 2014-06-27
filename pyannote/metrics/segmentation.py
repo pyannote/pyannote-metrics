384e4ff58c9b444d448c000e7ca61bf812d7f0d9
Herve Bredin <bredin@limsi.fr>
2014-06-27 12:46:25 +0200

    feat: add segmentation precision and recall

--- pyannote/metrics/segmentation.py
+++ pyannote/metrics/segmentation.py
@@ -23,6 +23,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# CONTRIBUTORS
+# Hervé Bredin -- http://herve.niderb.fr/
+# Camille Guinaudeau -- https://sites.google.com/site/cguinaudeau/
+# Mamadou Doumbia
+
 from __future__ import unicode_literals
 
 import numpy as np
@@ -38,6 +43,12 @@ PK_NAME = 'segmentation pk'
 WINDOWDIFF_NAME = 'segmentation windowdiff'
 PK_AGREEMENT = 'number of agreements'
 PK_COMPARISON = 'number of comparisons'
+
+PRECISION_NAME = 'segmentation precision'
+RECALL_NAME = 'segmentation recall'
+
+PR_BOUNDARIES = 'number of boundaries'
+PR_MATCHES = 'number of matches'
 
 
 class SegmentationCoverage(BaseMetric):
@@ -335,6 +346,158 @@ class SegmentationWindowDiff(BaseMetric):
         string += "  - number of agreements: %d\n" % (detail[PK_AGREEMENT])
         string += "  - %s: %.2f %%\n" % (self.name, 100 * detail[self.name])
         return string
+
+
+class SegmentationPrecision(BaseMetric):
+    """Segmentation precision
+
+    >>> from pyannote.core import Timeline, Segment
+    >>> from pyannote.metrics.segmentation import SegmentationPrecision
+    >>> precision = SegmentationPrecision()
+
+    >>> reference = Timeline()
+    >>> reference.add(Segment(0, 1))
+    >>> reference.add(Segment(1, 2))
+    >>> reference.add(Segment(2, 4))
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 1))
+    >>> hypothesis.add(Segment(1, 2))
+    >>> hypothesis.add(Segment(2, 3))
+    >>> hypothesis.add(Segment(3, 4))
+    >>> precision(reference, hypothesis)
+    0.6666666666666666
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 4))
+    >>> precision(reference, hypothesis)
+    1.0
+
+    """
+    @classmethod
+    def metric_name(cls):
+        return PRECISION_NAME
+
+    @classmethod
+    def metric_components(cls):
+        return [PR_MATCHES, PR_BOUNDARIES]
+
+    def __init__(self, tolerance=0., **kwargs):
+
+        super(SegmentationPrecision, self).__init__()
+        self.tolerance = tolerance
+
+    def _get_details(self, reference, hypothesis, **kwargs):
+
+        # extract timeline if needed
+        if isinstance(reference, Annotation):
+            reference = reference.get_timeline()
+        if isinstance(hypothesis, Annotation):
+            hypothesis = hypothesis.get_timeline()
+
+        detail = self._init_details()
+
+        # number of matches so far...
+        nMatches = 0.  # make sure it is a float (for later ratio)
+
+        # number of boundaries in reference and hypothesis
+        N = len(reference) - 1
+        M = len(hypothesis) - 1
+
+        # number of boundaries in hypothesis
+        detail[PR_BOUNDARIES] = M
+
+        # corner case (no boundary in hypothesis or in reference)
+        if M == 0 or N == 0:
+            detail[PR_MATCHES] = 0.
+            return detail
+
+        # reference and hypothesis boundaries
+        refBoundaries = [segment.end for segment in reference][:-1]
+        hypBoundaries = [segment.end for segment in hypothesis][:-1]
+
+        # temporal delta between all pairs of boundaries
+        delta = np.zeros((N, M))
+        for r, refBoundary in enumerate(refBoundaries):
+            for h, hypBoundary in enumerate(hypBoundaries):
+                delta[r, h] = abs(refBoundary - hypBoundary)
+
+        # make sure boundaries too far apart from each other cannot be matched
+        # (this is what np.inf is used for)
+        delta[np.where(delta > self.tolerance)] = np.inf
+
+        # h always contains the minimum value in delta matrix
+        # h == np.inf means that no boundary can be matched
+        h = np.amin(delta)
+
+        # while there are still boundaries to match
+        while h < np.inf:
+
+            # increment match count
+            nMatches += 1
+
+            # find boundaries to match
+            k = np.argmin(delta)
+            i = k / M
+            j = k % M
+
+            # make sure they cannot be matched again
+            delta[i, :] = np.inf
+            delta[:, j] = np.inf
+
+            # update minimum value in delta
+            h = np.amin(delta)
+
+        detail[PR_MATCHES] = nMatches
+        return detail
+
+    def _get_rate(self, detail):
+
+        numerator = detail[PR_MATCHES]
+        denominator = detail[PR_BOUNDARIES]
+
+        if denominator == 0.:
+            if numerator == 0:
+                return 1.
+            else:
+                raise ValueError('')
+        else:
+            return numerator / denominator
+
+
+class SegmentationRecall(SegmentationPrecision):
+    """Segmentation recall
+
+    >>> from pyannote.core import Timeline, Segment
+    >>> from pyannote.metrics.segmentation import SegmentationRecall
+    >>> recall = SegmentationRecall()
+
+    >>> reference = Timeline()
+    >>> reference.add(Segment(0, 1))
+    >>> reference.add(Segment(1, 2))
+    >>> reference.add(Segment(2, 4))
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 1))
+    >>> hypothesis.add(Segment(1, 2))
+    >>> hypothesis.add(Segment(2, 3))
+    >>> hypothesis.add(Segment(3, 4))
+    >>> recall(reference, hypothesis)
+    1.0
+
+    >>> hypothesis = Timeline()
+    >>> hypothesis.add(Segment(0, 4))
+    >>> recall(reference, hypothesis)
+    0.0
+
+    """
+    @classmethod
+    def metric_name(cls):
+        return RECALL_NAME
+
+    def _get_details(self, reference, hypothesis, **kwargs):
+        return super(SegmentationRecall, self)._get_details(
+            hypothesis, reference)
 
 
 if __name__ == "__main__":
