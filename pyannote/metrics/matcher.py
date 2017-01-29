7858db40f13e1dd9201656fd21fdd9a3ac61b30a
Hervé BREDIN <bredin@limsi.fr>
2017-01-29 14:59:11 +0100

    fix: make Matcher pickable

--- pyannote/metrics/matcher.py
+++ pyannote/metrics/matcher.py
@@ -159,22 +159,10 @@ class LabelMatcher(object):
         return (counts, details)
 
 
-class BaseMapper(object):
+class HungarianMapper(object):
 
-    def __init__(self, cost=None):
-        super(BaseMapper, self).__init__()
-        if cost is None:
-            cost = lambda AB: AB[0] * AB[1]
-        self.cost = cost
-
-    def __call__(self, A, B):
-        raise NotImplementedError()
-
-
-class HungarianMapper(BaseMapper):
-
-    def __init__(self, cost=None):
-        super(HungarianMapper, self).__init__(cost=cost)
+    def __init__(self):
+        super(HungarianMapper, self).__init__()
         self._munkres = Munkres()
 
     def _helper(self, A, B):
@@ -185,7 +173,7 @@ class HungarianMapper(BaseMapper):
         if Na > Nb:
             return {a: b for (b, a) in six.iteritems(self._helper(B, A))}
 
-        matrix = self.cost((A, B))
+        matrix = A * B
         mapping = self._munkres.compute(matrix.max() - matrix)
 
         return dict(
@@ -235,14 +223,11 @@ class HungarianMapper(BaseMapper):
         return mapping
 
 
-class GreedyMapper(BaseMapper):
-
-    def __init__(self, cost=None):
-        super(GreedyMapper, self).__init__(cost=cost)
+class GreedyMapper(object):
 
     def __call__(self, A, B):
 
-        matrix = self.cost((A, B))
+        matrix = A * B
         Na, Nb = matrix.shape
         N = min(Na, Nb)
 
