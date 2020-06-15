0b668e6c34fd6e6f4c436c7f78ff5169c8a34f2e
hadware <hadwarez@gmail.com>
2020-06-15 16:17:15 +0200

    Simplified the cli module into a single file.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'pyannote-metrics=pyannote.metrics.cli.pyannote_metrics:main',
+            'pyannote-metrics=pyannote.metrics.cli:main',
         ],
     },
     install_requires=[
